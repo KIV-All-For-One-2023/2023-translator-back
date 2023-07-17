1110f79b0b707c13934e776d0f05f68e67d83ccf
Judy <mjchoi0831@gmail.com>
2023-07-17 18:26:34 +0900

    Add : ctranslate

--- app/service/translate.py
+++ app/service/translate.py
@@ -16,8 +16,7 @@ async def translate_text(params) -> str:
     # if response.status_code != 200:
     #     return False
     
-    # 아직 MT 모델이 없으므로 기본 리턴 문자열 지정.
-    mt = "(mt)Watch me bring the fire and set the night alight"
+    mt = await get_mt(params)
     setattr(params, 'mt', mt)
     
     # 코드리뷰 요청
@@ -26,3 +25,20 @@ async def translate_text(params) -> str:
     await crud.create_translate(params)
 
     return mt
+
+import ctranslate2
+import sentencepiece as spm
+
+async def get_mt(params) -> str:
+
+    translator = ctranslate2.Translator(f"nmt/model/bin/{params.sl}-{params.tl}", device="cpu")
+    sp_sl = spm.SentencePieceProcessor(f"nmt/model/sentencepiece/sp_model.{params.sl}")
+    sp_tl = spm.SentencePieceProcessor(f"nmt/model/sentencepiece/sp_model.{params.tl}")
+
+    input_tokens = sp_sl.encode(params.text, out_type=str)
+    results = translator.translate_batch([input_tokens])
+
+    output_tokens = results[0].hypotheses[0]
+    output_text = sp_tl.decode(output_tokens)
+    
+    return output_text
